bf8c3c11e190e7556226321ebf5299dcca9b28ae
SolomashenkoRoman <solomashenko.roman.1991@gmail.com>
2015-07-15 22:58:34 +0300

    Создание заголовка плагина

--- SRomanDev.php
+++ SRomanDev.php
@@ -1,1 +1,10 @@
 <?php
+/*
+Plugin Name: SRomanDev
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: Lesson
+Version: 1.0
+Author: SRomanDev
+Author URI: http://URI_Of_The_Plugin_Author
+License: A "Slug" license name e.g. GPL2
+*/
