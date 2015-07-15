37bd08e21fa014cb4a124e16e744646acfb48c38
SolomashenkoRoman <solomashenko.roman.1991@gmail.com>
2015-07-15 23:40:15 +0300

    Создаем в основном файле класс для работы с хуками Активации, Деактивации и Удаления плагина

--- SRomanDev.php
+++ SRomanDev.php
@@ -8,3 +8,34 @@ Author: SRomanDev
 Author URI: http://URI_Of_The_Plugin_Author
 License: A "Slug" license name e.g. GPL2
 */
+if ( ! function_exists( 'add_action' ) ) exit;
+if( ! class_exists( 'SRomanDev' ) ) {
+    /**
+     * Class SRomanDev
+     */
+    final class SRomanDev{
+        public function __construct(){
+        }
+        //Активация плагина
+        public static function SRPluginActivation(){
+            error_log("Активация плагина");
+        }
+        //Деактивация плагина
+        public static function SRPluginDeactivation(){
+            error_log("Деактивация плагина");
+        }
+        //Удаление плагина
+        public static function SRPluginUninstall(){
+            error_log("Удаление плагина");
+        }
+    }
+}
+if( class_exists( 'Travelpayouts' ) ) {
+    $SRomanDev = new SRomanDev();
+    /**
+     * Добавляем базовые хуки при активации, деактивации и удалении плагина
+     **/
+    register_activation_hook( __FILE__,     array( 'SRomanDev',  'SRPluginActivation' ) );
+    register_deactivation_hook( __FILE__,   array( 'SRomanDev',  'SRPluginDeactivation' ) );
+    register_uninstall_hook( __FILE__,      array( 'SRomanDev',  'SRPluginUninstall' ) );
+}
